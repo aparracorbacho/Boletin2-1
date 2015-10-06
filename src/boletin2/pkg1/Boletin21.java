3df15345f1c6f0370989a0ff6ee475ab0febce29
aparracorbacho <aparracorbacho@atanasoff18.danielcastelao.org>
2015-10-06 08:55:07 +0200

    Boletin2-1

---
+++ src/boletin2/pkg1/Boletin21.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aparracorbacho
+ */
+public class Boletin21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float base, altura;
+        System.out.println("Introduce la base");
+        Scanner baseTeclado = new Scanner(System.in);
+        base = baseTeclado.nextFloat();
+        System.out.println("Introduce la altura");
+        Scanner alturaTeclado = new Scanner(System.in);
+        altura = alturaTeclado.nextFloat();
+        System.out.println("El area es: "+base*altura/2);
+        
+        
+        // TODO code application logic here
+    }
+    
+}
